6e5c610186e9ead5714f88a8fb95458a20b8fb47
zhazhjie <583714409@qq.com>
2020-03-23 13:39:29 +0800

    init

--- src/components/index.js
+++ src/components/index.js
@@ -4,12 +4,10 @@
  * @date: 2018-09-20 10:25:27
  * @version: 1.0
  */
-import permissionBtn from './permissionBtn';
 import tableTemplate from './tableTemplate';
 
 const plugin = Object.create(null);
 plugin.install = function (Vue) {
-  Vue.component(permissionBtn.name, permissionBtn);
   Vue.component(tableTemplate.name, tableTemplate);
 };
 if (typeof window !== 'undefined' && window.Vue) {
